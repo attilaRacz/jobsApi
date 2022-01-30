c752a786c4ff03abae04bf7794adbe6c4238653d
raczat <raczat@invitech.hu>
2022-01-30 18:02:26 +0100

    auto-increment entity id

--- src/main/java/com/example/jobsapi/model/Client.java
+++ src/main/java/com/example/jobsapi/model/Client.java
@@ -1,16 +1,13 @@
 package com.example.jobsapi.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="client")
 public class Client {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
--- src/main/java/com/example/jobsapi/model/Position.java
+++ src/main/java/com/example/jobsapi/model/Position.java
@@ -1,16 +1,13 @@
 package com.example.jobsapi.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="position")
 public class Position {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
