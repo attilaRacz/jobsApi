2f1f85fe6c1e9f4389d138b86cb0694ab9d7d662
raczat <raczat@invitech.hu>
2022-01-31 17:20:54 +0100

    return api key if client is registered

--- src/main/java/com/example/jobsapi/service/ClientService.java
+++ src/main/java/com/example/jobsapi/service/ClientService.java
@@ -2,7 +2,6 @@ package com.example.jobsapi.service;
 
 import com.example.jobsapi.dao.ClientRepository;
 import com.example.jobsapi.model.Client;
-import com.example.jobsapi.util.ConstraintViolation;
 import com.example.jobsapi.util.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,19 +19,19 @@ public class ClientService {
     private ClientRepository clientRepository;
 
     public UUID addClient(Client newClient) throws Exception {
-        Set<ConstraintViolation> violations = validator.validateClient(newClient);
+        Set<String> violations = validator.validateClient(newClient);
 
         if (!violations.isEmpty()) {
             // todo - find or implement right exception
             throw new Exception("Validation error: " + violations);
         }
 
-        clientRepository.save(newClient);
+        UUID apiKey = clientRepository.save(newClient).getId();
 
         // todo - should be a logger
         System.out.println("New client persisted: " + newClient);
 
         // todo - return UUID
-        return null;
+        return apiKey;
     }
 }
